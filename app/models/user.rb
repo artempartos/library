0ec41ed66780fb61818fdce31e0cdcb4f2518a0e
Seliverstov Maxim <maxim.web.deveoper@gmail.com>
2013-06-29 14:39:32 +0400

    Add user model and user_registration type

---
+++ app/models/user.rb
@@ -0,0 +1,23 @@
+class User < ActiveRecord::Base
+  has_secure_password
+
+  validates :email, uniqueness: true, presence: true, email: true
+
+  state_machine initial: :waiting_confirmation do
+    state :waiting_confirmation
+    state :inactive
+    state :active
+
+    event :confirm do
+      transition from: :waiting_confirmation, to: :active
+    end
+
+    event :deactivate do
+      transition from: :active, to: :inactive
+    end
+  end
+
+  def to_s
+    email
+  end
+end
