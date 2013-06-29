d1e3f546f976da69803fbc58ac59bbf2bba03aea
aelaa <aelaa@aelaa-N100.(none)>
2013-06-30 00:14:07 +0400

    books locales

--- app/controllers/web/application_controller.rb
+++ app/controllers/web/application_controller.rb
@@ -1,5 +1,6 @@
 class Web::ApplicationController < ApplicationController
   protect_from_forgery with: :exception
 
-  before_filter :authenticate_user!
+  before_filter :authenticate_admin!
+  skip_before_filter :authenticate_user!
 end
