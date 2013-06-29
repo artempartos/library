36ddcd8f70be7afbfd27c601e8008b6e2a0fa3dc
Oleg Chebulaev <alegch@undev.ru>
2013-06-29 20:00:04 +0400

    database yml

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,7 +9,7 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  ActiveRecord::Migration.check_pending!
+  # ActiveRecord::Migration.check_pending!
 
   include FactoryGirl::Syntax::Methods
   include AuthHelper
