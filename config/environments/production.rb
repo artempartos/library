41be165b36bfcec52aa4d5a15e58faeffc5c2637
Artem Petrov <partos0511@gmail.com>
2013-09-01 15:58:46 +0400

    add assets config for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,6 +27,7 @@ Library::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
+
   config.assets.compile = false
 
   # Generate digests for assets URLs.
@@ -38,7 +39,7 @@ Library::Application.configure do
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
-
+  config.assets.initialize_on_precompile = false
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
