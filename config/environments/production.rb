90d67e5a21199c52cee4e72334d822670817f341
Artem Petrov <partos0511@gmail.com>
2013-09-01 16:16:07 +0400

    change production asset config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,14 +20,13 @@ Library::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-
   config.assets.compile = false
 
   # Generate digests for assets URLs.
@@ -39,7 +38,7 @@ Library::Application.configure do
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
-  config.assets.initialize_on_precompile = false
+
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
