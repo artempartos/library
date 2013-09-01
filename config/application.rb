5ec704822e1be4d510799a6e17632a240efe6cb5
Artem Petrov <partos0511@gmail.com>
2013-09-01 16:51:57 +0400

    add app config

--- config/application.rb
+++ config/application.rb
@@ -2,22 +2,28 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
+
+load 'config/configus.rb'
 
 module Library
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.i18n.default_locale = :ru
+    config.time_zone = 'Moscow'
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.encoding = "utf-8"
+
+    config.filter_parameters += [:password]
+
+    config.generators do |g|
+      g.template_engine :haml
+      g.stylesheets = false
+      g.javascripts = false
+      g.helper false
+    end
+
+    config.exceptions_app = self.routes
   end
 end
