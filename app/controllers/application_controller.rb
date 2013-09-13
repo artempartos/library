7aafc7513187565c0312cf982f6596b75cb217a7
Artem Petrov <partos0511@gmail.com>
2013-09-14 00:27:43 +0400

    update controllers for session/app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
   include Concerns::AuthManagment
+  include Concerns::FlashHelper
 
   helper_method :current_user, :signed_in?
+
+  class << self
+    def url_helpers(route_name, *args)
+      Rails.application.routes.url_helpers.send route_name, args
+    end
+  end
+
 end
