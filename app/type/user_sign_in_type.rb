7febe2047e9060d02c98b014cc7d73e5c58532e7
Artem Petrov <partos0511@gmail.com>
2013-09-14 00:26:23 +0400

    add types

---
+++ app/type/user_sign_in_type.rb
@@ -0,0 +1,38 @@
+class UserSignInType
+  include ApplicationTypeWithoutActiveRecord
+
+  attribute :email, String
+  attribute :password, String
+
+  validates :email, presence: true, email: true
+  validates :password, presence: true
+
+  validate :check_authenticate, if: :email
+  validate :check_bunned_user, if: :email
+  validate :check_new_user, if: :email
+
+  def user
+    ::User.where(email: email.mb_chars.downcase).first
+  end
+
+  private
+
+  def check_authenticate
+    if !user.try(:authenticate, password)
+      errors.add(:password, :user_or_password_invalid)
+    end
+  end
+
+  def check_bunned_user
+    if user.try(:banned?)
+      errors.add(:email, :user_inactive)
+    end
+  end
+
+  def check_new_user
+    if user.try(:waiting_confirmation?)
+      errors.add(:email, :user_new)
+    end
+  end
+
+end
