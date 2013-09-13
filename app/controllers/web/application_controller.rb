e660549ce2001cc04d902152922130de85069056
Artem Petrov <partos0511@gmail.com>
2013-09-13 21:49:52 +0400

    change app controller

--- app/controllers/web/application_controller.rb
+++ app/controllers/web/application_controller.rb
@@ -1,2 +1,9 @@
 class Web::ApplicationController < ApplicationController
+
+  protect_from_forgery with: :exception
+
+  def redirect_to_back_or(default)
+    redirect_to params[:from] || default
+  end
+
 end
