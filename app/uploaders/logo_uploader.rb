0458435f26ad0947b35b01eee13fa9cf22181a5a
Alexey Chuvashov <alexeychu@gmail.com>
2013-09-14 05:17:47 +0400

    change base uploader

--- app/uploaders/logo_uploader.rb
+++ app/uploaders/logo_uploader.rb
@@ -1,12 +1,8 @@
-class LogoUploader < BaseUploader
+class LogoUploader < ApplicationUploader
   process :resize_to_fit => [200, 200]
 
   version :thumb do
     process :resize_to_fill => [30,30]
-  end
-
-  def extension_white_list
-     %w(jpg jpeg gif png)
   end
 
 end
