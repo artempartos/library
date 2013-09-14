1b81589f6fcb664b9ec198a8fea26b6df4ef680f
Alexey Chuvashov <alexeychu@gmail.com>
2013-09-14 04:26:07 +0400

    add image processing

---
+++ app/uploaders/base_uploader.rb
@@ -0,0 +1,11 @@
+class BaseUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  storage :file
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+
+end
